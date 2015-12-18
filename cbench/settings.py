1e873f933f3d467f69e5d1e629bb38f4c88adccb
joalder <jonas.alder@gmail.com>
2015-12-18 19:59:38 +0100

    - removed default key

--- cbench/settings.py
+++ cbench/settings.py
@@ -2,4 +2,4 @@ DEFAULT_INSTANCE_TYPE = "t2.micro"
 DEFAULT_INSTANCE_IMANGE = "ami-accff2b1"
 DEFAULT_KEY_NAME = "jonas_public"
 
-KEY_FILE = "C:\\Users\\Jonas\\.ssh\\jonas_private.ppk"
+KEY_FILE = ""
